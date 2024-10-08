c9421e3150dfdbb71185eae135d51b05fc4d8662
mike <michaelmussa9@gmail.com>
2024-10-09 02:08:39 +0300

    Implement Image upload service, image upload service

--- src/main/java/com/mike/shoppingcart/service/images/ImageService.java
+++ src/main/java/com/mike/shoppingcart/service/images/ImageService.java
@@ -3,13 +3,15 @@ package com.mike.shoppingcart.service.images;
 import com.mike.shoppingcart.model.Image;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface ImageService {
 
     Image getImageById(Long id);
 
     void deleteImageByID(Long id);
 
-    Image saveImage(MultipartFile file, Long productId);
+    Image saveImage(List<MultipartFile> file, Long productId);
 
     void updateImage(MultipartFile file,Long ImageId);
 
--- src/main/java/com/mike/shoppingcart/service/images/impl/ImageServiceimpl.java
+++ src/main/java/com/mike/shoppingcart/service/images/impl/ImageServiceimpl.java
@@ -1,17 +1,30 @@
 package com.mike.shoppingcart.service.images.impl;
 
+import com.mike.shoppingcart.dto.ImageDto;
 import com.mike.shoppingcart.exceptions.ProductNotFoundException;
 import com.mike.shoppingcart.exceptions.ResourceNotFoundException;
 import com.mike.shoppingcart.model.Image;
+import com.mike.shoppingcart.model.Product;
 import com.mike.shoppingcart.reposistory.ImageRepository;
 import com.mike.shoppingcart.service.images.ImageService;
+import com.mike.shoppingcart.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageServiceimpl implements ImageService {
 
     @Autowired
     private ImageRepository imageRepository;
+
+
+    @Autowired
+    private ProductService productService;
 
     @Override
     public Image getImageById(Long id) {
@@ -28,12 +41,45 @@ public class ImageServiceimpl implements ImageService {
     }
 
     @Override
-    public Image saveImage(MultipartFile file, Long productId) {
+    public Image saveImage(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+
+        List<ImageDto> imageDtos = new ArrayList<>();
+        for(MultipartFile file:files){
+
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+                String downloadUrl = "/api/v1/images/image/download/"+image.getId();
+                image.setDownloadUrl(downloadUrl);
+                Image savedimage =imageRepository.save(image);
+                savedimage.setDownloadUrl("/api/v1/images/image/download/"+image.getId());
+
+            } catch (SQLException | IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
         return null;
     }
 
     @Override
-    public void updateImage(MultipartFile file, Long ImageId) {
+    public void updateImage(MultipartFile file, Long imageId) {
+        Image image = getImageById(imageId);
+
+        try {
+            image.setFileName(file.getOriginalFilename());
+//            image.se(file.getOriginalFilename());
+            image.setImage(new SerialBlob(file.getBytes()));
+            imageRepository.save(image);
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
 
     }
 }
