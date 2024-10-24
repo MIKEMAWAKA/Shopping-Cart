353b413f6974bd862e86809d0f2da95a199bc715
mike <michaelmussa9@gmail.com>
2024-10-24 17:14:35 +0300

    cart model

---
+++ src/main/java/com/mike/shoppingcart/model/Cart.java
@@ -0,0 +1,25 @@
+package com.mike.shoppingcart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount=BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL)
+    private Set<CartItem > cartItems;
+}
